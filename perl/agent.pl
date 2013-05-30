2df5575ebf47976566a4bc1424b6a5a447fed2af
Rafael Casado Sanchez <rafacas@gmail.com>
2013-05-30 22:31:33 +0100

    Check CPU Load Average

--- perl/agent.pl
+++ perl/agent.pl
@@ -1,8 +1,27 @@
 #!/usr/bin/env perl
 
-# CPU Load Average: /proc/loadavg
+use strict;
+use warnings;
+
+
+# Check CPU Load Average
+# /proc/loadavg 
+if ($^O eq 'linux'){
+    open my $la, '<', '/proc/loadavg';
+    print <$la>;
+    close $la;
+} else {
+    print "Unsupported platform: $^O\n";
+}
+
 # Memory & swap: /proc/meminfo
 # Network traffic: /proc/net/dev
 # CPU stats: mpstat
 # Disk usage: df
 # IO stats: iostat
+
+
+# Send data in JSON
+# http://search.cpan.org/~makamaka/JSON-2.58/lib/JSON.pm
+# JSON::PP has been a core module since perl 5.14
+# perl version check: if ($] < 5.008 )
