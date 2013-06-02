37052eeb4a4dfc5b9211469bdd7d2606f29d3417
Rafael Casado Sanchez <rafacas@gmail.com>
2013-06-02 22:49:12 +0100

    Adding debug messages

--- perl/agent.pl
+++ perl/agent.pl
@@ -2,41 +2,27 @@
 
 use strict;
 use warnings;
+use Log::Message::Simple;
 
+# Logging options
+my $verbose = 1;
+my $debug = 1;
 
 # Check CPU Load Average
-my $loadavg;
-if ($^O eq 'linux'){
-    open LOADAVG, '<', '/proc/loadavg'; # or die
-    $loadavg = <LOADAVG>;
-    chomp $loadavg;
-    close LOADAVG;
-} else {
-    print "Unsupported platform: $^O\n";
-}
-
-print "$loadavg\n";
+getLoadAvg();
 
 # Check Memory & swap
-if ($^O eq 'linux'){
-    open MEM, '<', '/proc/meminfo';
-#    print <$mem>;
-    close MEM;
-} else {
-    print "Unsupported platform: $^O\n";
-}
+getMemory();
 
 # Network traffic: /proc/net/dev
-if ($^O eq 'linux'){
-    open NET, '<', '/proc/net/dev';
-#    print <$net>;
-    close NET;
-} else {
-    print "Unsupported platform: $^O\n";
-}
+getNetwork();
 
 # CPU stats: mpstat
+
+
 # Disk usage: df
+
+
 # IO stats: iostat
 
 
@@ -44,3 +30,44 @@ if ($^O eq 'linux'){
 # http://search.cpan.org/~makamaka/JSON-2.58/lib/JSON.pm
 # JSON::PP has been a core module since perl 5.14
 # perl version check: if ($] < 5.008 )
+
+# SUBROUTINES
+
+sub getLoadAvg {
+    debug("getLoadAvg: start", $debug);
+    my $loadavg;
+    if ($^O eq 'linux'){
+        debug("getLoadAvg: linux", $debug);
+        debug("getLoadAvg: opening /proc/loadavg file", $debug);
+        open LOADAVG, '<', '/proc/loadavg'; # or die
+        $loadavg = <LOADAVG>;
+        chomp $loadavg;
+        debug("getLoadAvg: loadavg -> $loadavg", $debug);
+        close LOADAVG;
+    } else {
+        debug("Unsupported platform: $^O", $debug);
+    }
+
+    debug("getLoadAvg: completed", $debug);
+
+}
+
+sub getMemory {
+    if ($^O eq 'linux'){
+        open MEM, '<', '/proc/meminfo';
+        #print <$mem>;
+        close MEM;
+    } else {
+        print "Unsupported platform: $^O\n";
+    }
+}
+
+sub getNetwork {
+    if ($^O eq 'linux'){
+        open NET, '<', '/proc/net/dev';
+        #print <$net>;
+        close NET;
+    } else {
+        print "Unsupported platform: $^O\n";
+    }
+}
